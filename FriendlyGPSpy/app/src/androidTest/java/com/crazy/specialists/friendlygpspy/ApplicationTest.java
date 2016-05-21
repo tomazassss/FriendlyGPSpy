bb6f43dfe4c120886f0849b7b277514c8a891058
Tomas <tomazassss@gmail.com>
2016-05-21 23:49:22 +0300

    Initial commit for Android studio

---
+++ FriendlyGPSpy/app/src/androidTest/java/com/crazy/specialists/friendlygpspy/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.crazy.specialists.friendlygpspy;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
